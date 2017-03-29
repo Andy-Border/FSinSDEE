de32ce62b704485d03c5bd4076dc176d94b6880e
Andy-Border <zja00999@gmail.com>
2017-03-29 23:14:09 +0200

    infotheo package aded

--- V1/main.R
+++ V1/main.R
@@ -3,6 +3,7 @@
 #Part 1: Data Preparation
 rm(list = ls())
 setwd("D:/R/FSinSDEEV2")
+library("dplyr")
 source("import_ISBSG.R")
 source("filter_projects.R")
 #source("select_features.R")
@@ -13,13 +14,16 @@ source("mRMR.R")
 source("All_Functions.R")
 dData = ISBSG
 responseVNum  <-  which(names(ISBSG) %in% "Normalised.Work.Effort.Level.1")
-sData=ISBSG[,-responseVNum]
+dData=mutate(dData,ResponseVariable=Normalised.Work.Effort.Level.1)
+dData=dData[,-responseVNum]
+responseVNum <- ncol(dData)
+sData=dData[,-responseVNum]
 entp <- getEntropy(sData)
-
-getMatrixs(data = ISBSG,entp = entp)
+vecColType <- c(1,1,1,1,1,1,0,0,0,1,1)
+#getMatrixs(data = dData,entp = entp)
 
 #Part 2: Feature Selection
-resultOfFS<- mRMR(dData = ISBSG, rFNum = responseVNum,k=ncol(dData)-1)
+resultOfFS<- mRMR(dData = dData, rFNum = ncol(dData),k=ncol(dData)-1)
 vecBestSubset <- resultOfFS$S
 
 weight <- getFeatureWeights_None(vecBestSubset)
@@ -29,65 +33,55 @@ kFoldnum=3
 
 #for(evNum in 1:kFoldnum){
 ##Seperate input data into two parts: TrainingSet and TestingSet
-rtnList <- seperateSets(dData, kFoldNbr)
-vecTestingIds = rtnList$var1
-vecTrainingIds = rtnList$var2
-testingData <-
-  matrix(nrow = length(vecTestingIds), ncol = ncol(dData))
-trainingData <-
-  matrix(nrow = length(vecTrainingIds), ncol = ncol(dData))
+rtnList <- seperateSets(dData, kFoldnum)
+testingData <- dData[rtnList$var1,]
+trainingData <-dData[rtnList$var2,]
 
-for (i in 1:length(vecTestingIds)) {
-  testingData[i, ] = dData[vecTestingIds[i], ]
-}
-for (i in 1:length(vecTrainingIds)) {
-  trainingData[i, ] = dData[vecTrainingIds[i], ]
-}
 #evaluate testing set
-evaluation <-
-  EvalTesting(testingData, trainingData, weight, vecBestSubset)
+evaluation <-EvalTesting(
+  testingData, trainingData, 
+  weight, vecBestSubset,
+  vecColType)
 
 vecMMRE = 0
 vecPRED = 0
 vecMdMRE = 0
 
 evaluation <-
-  EvalTesting(testingData, trainingData, weight, vecBestSubset)
+  EvalTesting(testingData, trainingData, weight, vecBestSubset,vecColType)
 
 #collect the experiment results
-result <- vector(length = 3)
+result <- NULL
 
-result[1] = MMREFunc(evaluation, nrow(testingData))
-
-result[2] = PREDFunc(evaluation, nrow(testingData), 0.25)
-
-result[3] = MdMREFunc(evaluation, nrow(testingData))
-
+result$MMRE = MMREFunc(evaluation, nrow(testingData))
+result$PRED = PREDFunc(evaluation, nrow(testingData), 0.25)
+result$MdMRE = MdMREFunc(evaluation, nrow(testingData))
+print(result)
 #print("TestingSet Result:")
 #print(result);
-vecMMRE[z] <- result[1]
-
-vecPRED[z] <- result[2]
-
-vecMdMRE[z] <- result[3]
-
-
-#Part 4: Case Selection
-#Part 5: Case Adaption
-#Part 6: Evaluation
-#}
-print(vecMMRE)
-
-print("PREDs:")
-print(vecPRED)
-
-print("MdMREs:")
-print(vecMdMRE)
-
-print("Average in MMRE:")
-print(mean(vecMMRE))
-print("Average in PRED:")
-print(mean(vecPRED))
-print("Average in MdMRE:")
-print(mean(vecMdMRE))
+# # vecMMRE[z] <- result[1]
+# # 
+# # vecPRED[z] <- result[2]
+# # 
+# # vecMdMRE[z] <- result[3]
+# # 
+# 
+# #Part 4: Case Selection
+# #Part 5: Case Adaption
+# #Part 6: Evaluation
+# #}
+# print(vecMMRE)
+# 
+# print("PREDs:")
+# print(vecPRED)
+# 
+# print("MdMREs:")
+# print(vecMdMRE)
+# 
+# print("Average in MMRE:")
+# print(mean(vecMMRE))
+# print("Average in PRED:")
+# print(mean(vecPRED))
+# print("Average in MdMRE:")
+# print(mean(vecMdMRE))
 
